09e3707c9161d5da5ab67fd64432c957006401e2
Игнатович <ignatovich.ei@gmail.com>
2019-02-25 16:24:59 +0600

    2-module: 1,2 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @returns {Object}
  */
 function clone (obj) {
-}
+    let newObj = {};
+    for(let key in obj) {
+        if (typeof(obj[key])==='object' && obj[key]!==null) {
+            newObj[key] = clone(obj[key]);
+        } else{
+            newObj[key] = obj[key];
+        }
+    }
+    return newObj;
+} 
+   
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,8 +1,40 @@
+function a(obj, path = '') {
+    const result = {};
+    for(let key in obj) {
+      if (typeof(obj[key])==='object') {
+        let newpath
+        if (path === '') {
+          newpath = a(obj[key], path + key);  
+        } else {
+          newpath = a(obj[key], path + '.' + key); 
+        }
+        Object.assign(result, newpath);
+      } else {
+        result[path + '.' + key]= obj[key];
+      }
+    }
+    return result;
+}
+
 /**
  * Клонируем объект
  * @param {Object} obj - объект в котором ищем
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
-}
+function find(obj, value) {
+    const result = a(obj);
+    const arr = [];
+    for(let key in result) {
+      let i = arr.length;
+      if (result[key]===value) {
+        arr[i] = key;
+      }
+    }
+    if (arr.length === 0) {
+      return null;
+    } if (arr.length === 1){
+      return arr[0];
+    }
+    return arr;
+  }
