1781838133952d0a887a8084ca97d549d33150fc
Игнатович <ignatovich.ei@gmail.com>
2019-02-26 16:57:33 +0600

    3 module -1,2 tasks

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,38 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+  let current = this.from;
+  let last = this.to;
+  return {
+    next() {
+      if (current <= last) {
+        if(current.getDay()===5 || current.getDay()===6){
+          current.setDate(current.getDate() + 1);
+          return current.getDate()<10?{
+            done: false,
+            value: `[0${current.getDate(current.setDate(current.getDate()))}]`
+          }:{
+            done: false,
+            value: `[${current.getDate(current.setDate(current.getDate()))}]`
+          };
+        }
+        else {
+            current.setDate(current.getDate() + 1);
+            return current.getDate()<10?{
+                done: false,
+                value: `0${current.getDate(current.setDate(current.getDate()))}`
+            }:{
+                done: false,
+                value: `${current.getDate(current.setDate(current.getDate()))}`
+            };
+        }
+      } else {
+        return {
+          done: true
+        };
+      }
+    }
+  }
 };
 
 
